bec1dee10b22986ce353ca87f02a8af3be1d3981
JP <jprshaid123@gmail.com>
2021-04-17 16:27:01 -0300

    Camera Jitter
    Fixed Camera jitter when the player is running

--- Assets/Scripts/Camera/CameraDirection.cs
+++ Assets/Scripts/Camera/CameraDirection.cs
@@ -4,44 +4,22 @@ using UnityEngine;
 
 public class CameraDirection : MonoBehaviour
 {
-    
-    /*public      GameObject      cameraTarget;
-    public      Vector3         distanceFromObject;
-    public      float           smoothingSpeed = 0.2f;
-    public      float           cameraMovementSpeed;
 
+    public float smoothingTime = 0.15f;
+    private Vector3 velocity = Vector3.zero;
+    public Transform target;
+    public Camera camera;
 
-    void Update()
+    // Update is called once per frame
+    void FixedUpdate()
     {
-        transform.LookAt(cameraTarget.transform);
-        checkMouseInput();
-        checkCameraPosition();
-    }
-
-    private void LateUpdate()
-    {
-        Vector3 newPosition = cameraTarget.transform.position + distanceFromObject;
-        Vector3 smoothTransition = Vector3.Lerp(transform.position, newPosition, smoothingSpeed);
-        transform.position = smoothTransition;
-    }
-
-    public void checkMouseInput()
-    {
-        if (Input.GetAxis("Mouse X") > 0)
+        if (target)
         {
-            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * cameraMovementSpeed,
-                                       0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * cameraMovementSpeed);
+            Vector3 point = camera.WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.35f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, smoothingTime);
         }
 
-        else if (Input.GetAxis("Mouse X") < 0)
-        {
-            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * cameraMovementSpeed,
-                                       0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * cameraMovementSpeed);
-        }
     }
-
-    public void checkCameraPosition()
-    {
-
-    }*/
 }
---
+++ Assets/Scripts/Camera/TeleportToPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportToPlayer : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothingTime = 0.005f;
+    
+    void FixedUpdate()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, target.position, smoothingTime);
+    }
+}
